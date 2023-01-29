2eba96e1ce14cd140bf7da3247d88f4653b20cd8
daniel <danielnsuemartinez@gmail.com>
2023-01-29 02:19:15 +0100

    visual selector pesonaje

--- app/src/main/java/com/example/espacia2/SelectorPersonaje.java
+++ app/src/main/java/com/example/espacia2/SelectorPersonaje.java
@@ -102,10 +102,6 @@ public class SelectorPersonaje extends AppCompatActivity {
 
 
 
-
-
-
-
                 }else{
                     Context context = getApplicationContext();
                     CharSequence text = "No puedes elegir el mismo avatar";
@@ -139,11 +135,22 @@ public class SelectorPersonaje extends AppCompatActivity {
             public void onClick(View view) {
                 if(!seleccionado){
                     avatar1 = "personaje1";
+
                 }else{
                     avatar2 = "personaje1";
                 }
+
+                personaje1.setImageResource(R.drawable.personaje1);
+                personaje2.setImageResource(R.drawable.personaje2seleccionado);
+                personaje3.setImageResource(R.drawable.personaje3seleccionado);
+                personaje4.setImageResource(R.drawable.personaje4seleccionado);
+                personaje5.setImageResource(R.drawable.personaje5seleccionado);
+                personaje6.setImageResource(R.drawable.personaje6seleccionado);
+
+                comrpobarAvatar(avatar1);
                 mediaPlayer.seekTo(1000);
                 mediaPlayer.start();
+
 
             }
         });
@@ -152,9 +159,24 @@ public class SelectorPersonaje extends AppCompatActivity {
             public void onClick(View view) {
                 if(!seleccionado){
                     avatar1 = "personaje2";
+
                 }else{
+
                     avatar2 = "personaje2";
                 }
+
+
+
+
+                personaje2.setImageResource(R.drawable.personaje2);
+                personaje1.setImageResource(R.drawable.personaje1seleccionado);
+                personaje3.setImageResource(R.drawable.personaje3seleccionado);
+                personaje4.setImageResource(R.drawable.personaje4seleccionado);
+                personaje5.setImageResource(R.drawable.personaje5seleccionado);
+                personaje6.setImageResource(R.drawable.personaje6seleccionado);
+                comrpobarAvatar(avatar1);
+
+
                 mediaPlayer.seekTo(1000);
                 mediaPlayer.start();
             }
@@ -167,6 +189,17 @@ public class SelectorPersonaje extends AppCompatActivity {
                 }else{
                     avatar2 = "personaje3";
                 }
+
+
+                personaje3.setImageResource(R.drawable.personaje3);
+                personaje1.setImageResource(R.drawable.personaje1seleccionado);
+                personaje2.setImageResource(R.drawable.personaje2seleccionado);
+                personaje4.setImageResource(R.drawable.personaje4seleccionado);
+                personaje5.setImageResource(R.drawable.personaje5seleccionado);
+                personaje6.setImageResource(R.drawable.personaje6seleccionado);
+
+
+                comrpobarAvatar(avatar1);
                 mediaPlayer.seekTo(1000);
                 mediaPlayer.start();
             }
@@ -179,6 +212,16 @@ public class SelectorPersonaje extends AppCompatActivity {
                 }else{
                     avatar2 = "personaje4";
                 }
+
+
+
+                personaje4.setImageResource(R.drawable.personaje4);
+                personaje1.setImageResource(R.drawable.personaje1seleccionado);
+                personaje2.setImageResource(R.drawable.personaje2seleccionado);
+                personaje3.setImageResource(R.drawable.personaje3seleccionado);
+                personaje5.setImageResource(R.drawable.personaje5seleccionado);
+                personaje6.setImageResource(R.drawable.personaje6seleccionado);
+                comrpobarAvatar(avatar1);
                 mediaPlayer.seekTo(1000);
                 mediaPlayer.start();
             }
@@ -191,6 +234,16 @@ public class SelectorPersonaje extends AppCompatActivity {
                 }else{
                     avatar2 = "personaje5";
                 }
+
+                personaje5.setImageResource(R.drawable.personaje5);
+                personaje1.setImageResource(R.drawable.personaje1seleccionado);
+                personaje2.setImageResource(R.drawable.personaje2seleccionado);
+                personaje3.setImageResource(R.drawable.personaje3seleccionado);
+                personaje4.setImageResource(R.drawable.personaje4seleccionado);
+                personaje6.setImageResource(R.drawable.personaje6seleccionado);
+
+                comrpobarAvatar(avatar1);
+
                 mediaPlayer.seekTo(1000);
                 mediaPlayer.start();
             }
@@ -203,6 +256,16 @@ public class SelectorPersonaje extends AppCompatActivity {
                 }else{
                     avatar2 = "personaje6";
                 }
+
+
+                personaje6.setImageResource(R.drawable.perosnaje6);
+                personaje1.setImageResource(R.drawable.personaje1seleccionado);
+                personaje2.setImageResource(R.drawable.personaje2seleccionado);
+                personaje3.setImageResource(R.drawable.personaje3seleccionado);
+                personaje4.setImageResource(R.drawable.personaje4seleccionado);
+                personaje5.setImageResource(R.drawable.personaje5seleccionado);
+
+                comrpobarAvatar(avatar1);
                 mediaPlayer.seekTo(1000);
                 mediaPlayer.start();
             }
@@ -215,22 +278,22 @@ public class SelectorPersonaje extends AppCompatActivity {
 
         switch (avatar){
             case "personaje1":
-                personaje1.setImageResource(R.drawable.personaje1seleccionado);
+                personaje1.setImageResource(R.drawable.personaje1);
                 break;
             case "personaje2":
-                personaje2.setImageResource(R.drawable.personaje2seleccionado);
+                personaje2.setImageResource(R.drawable.personaje2);
                 break;
             case "personaje3":
-                personaje3.setImageResource(R.drawable.personaje3seleccionado);
+                personaje3.setImageResource(R.drawable.personaje3);
                 break;
             case "personaje4":
-                personaje4.setImageResource(R.drawable.personaje4seleccionado);
+                personaje4.setImageResource(R.drawable.personaje4);
                 break;
             case "personaje5":
-                personaje5.setImageResource(R.drawable.personaje5seleccionado);
+                personaje5.setImageResource(R.drawable.personaje5);
                 break;
             case "personaje6":
-                personaje6.setImageResource(R.drawable.personaje6seleccionado);
+                personaje6.setImageResource(R.drawable.perosnaje6);
                 break;
         }
 
